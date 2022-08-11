8ab967482e1b77c4595768585de9ee698b01d4fd
senkentarou <masahiro-senda@c-fo.com>
2022-08-11 23:52:30 +0900

    :heart: fix fatal bug

--- lua/gopr.lua
+++ lua/gopr.lua
@@ -40,7 +40,6 @@ function gopr.open_git_pull_request(args)
   -- detect commit hash on current line
   local current_file = vim.fn.expand("%")
   local current_line = vim.fn.line('.')
-
   if #current_file <= 0 or current_line <= 0 then
     print('fatal: could not find file or line.')
     return
@@ -48,10 +47,15 @@ function gopr.open_git_pull_request(args)
 
   local blame_hash = run('git blame -L ' .. current_line .. ',' .. current_line .. ' ' .. current_file .. ' | cut -d\' \' -f1')
   local commit_hash = string.gsub(blame_hash, '[^0-9a-f]', '')
+  if string.find(commit_hash, '^0+$') then
+    print('fatal: current line is not commited yet.')
+    return
+  end
 
   -- detect pr number
   local commit_range = commit_hash .. '...HEAD'
-  local pr_number = run('git log --merges --oneline --reverse --ancestry-path ' .. commit_range .. ' | grep -o "#[0-9]*" -m 1 | sed s/#//g')
+  local detect_merged_pr = run('git log --merges --oneline --reverse --ancestry-path ' .. commit_range .. ' | grep -o "#[0-9]*" -m 1 | sed s/#//g')
+  local pr_number = string.gsub(detect_merged_pr, '%s+', '')
 
   -- detect remote url
   local target_remote = vim.g.gopr.default_remote
@@ -69,14 +73,17 @@ function gopr.open_git_pull_request(args)
   --  https://github.com/senkentarou/gopr.nvim.git => senkentarou/gopr.nvim
   --  git@github.com:senkentarou/gopr.nvim.git     => senkentarou/gopr.nvim
   local git_remote_url = run('git ls-remote --get-url ' .. target_remote)
-  local target_url_base = string.gsub(git_remote_url, '^.-' .. vim.g.gopr.remote_base_url .. '[:/]?(.*)%.git%s?$', '%1')
-  if #pr_number <= 0 or git_remote_url == target_url_base or #target_url_base <= 0 then
-    print('fatal: could not find pr or remote url.')
+  local url_base = string.gsub(git_remote_url, '^.-' .. vim.g.gopr.remote_base_url .. '[:/]?(.*)%.git%s?$', '%1')
+  if #pr_number <= 0 or git_remote_url == url_base or #url_base <= 0 then
+    print('fatal: could not find pr or remote url by commit hash: ' .. commit_hash)
     return
   end
 
   -- open pull request
-  os.execute('open https://' .. vim.g.gopr.remote_base_url .. '/' .. target_url_base .. '/pull/' .. pr_number)
+  local target_url = 'https://' .. vim.g.gopr.remote_base_url .. '/' .. url_base .. '/pull/' .. pr_number
+  os.execute('open ' .. target_url)
+
+  print('opened: ' .. target_url .. ' on ' .. commit_hash)
 end
 
 return gopr
