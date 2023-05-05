19769455c47f57c8976f3fd8167ade6bd978efa7
senkentarou <masahiro-senda@c-fo.com>
2023-05-06 00:36:23 +0900

    introduce vim.notify

--- lua/gopr.lua
+++ lua/gopr.lua
@@ -11,27 +11,9 @@ local function run(command)
     handle:close()
 
     return string.gsub(result, '\n', ' ')
-  else
-    return ''
   end
-end
 
-local function exists(path)
-  local file = io.open(path, "r")
-
-  if file then
-    io.close(file)
-    return true
-  else
-    return false
-  end
-end
-
-local function check_git()
-  -- check .git repository
-  if not exists('.git') then
-    error('fatal: .git repository does not exist.')
-  end
+  return ''
 end
 
 local function current_commit_hash()
@@ -39,12 +21,14 @@ local function current_commit_hash()
   local current_file = vim.fn.expand("%")
   local current_line = vim.fn.line('.')
   if #current_file <= 0 or current_line <= 0 then
-    error('fatal: could not find file or line.')
+    vim.notify('fatal: could not find file or line.', vim.log.levels.ERROR)
+    return
   end
 
   local blame_hash = string.gsub(run('git blame -L ' .. current_line .. ',' .. current_line .. ' ' .. current_file .. ' | cut -d\' \' -f1'), '[^0-9a-f]', '')
   if string.find(blame_hash, '^0+$') then
-    error('fatal: current line is not commited yet.')
+    vim.notify('fatal: current line is not commited yet.', vim.log.levels.ERROR)
+    return
   end
 
   return string.gsub(run('git log --pretty=%H -1 ' .. blame_hash), '%s+', '')
@@ -64,7 +48,8 @@ local function remote_base_url(args)
   local git_remote_url = run('git ls-remote --get-url ' .. target_remote)
   local url_base = string.gsub(git_remote_url, '^.-github.com[:/]?(.*)%.git%s?$', '%1')
   if git_remote_url == url_base or #url_base <= 0 then
-    error('fatal: could not open remote url about \'' .. git_remote_url .. '\'')
+    vim.notify('fatal: could not open remote url about \'' .. git_remote_url .. '\'', vim.log.levels.ERROR)
+    return
   end
 
   return url_base
@@ -81,13 +66,12 @@ end
 
 -- open pull request directly
 function gopr.open_git_pull_request(args)
-  check_git()
-
   local target_commit = current_commit_hash()
 
   local target_pr = string.gsub(run('git log --merges --oneline --reverse --ancestry-path ' .. target_commit .. '...HEAD' .. ' | grep -o "#[0-9]*" -m 1 | sed s/#//g'), '%s+', '')
   if #target_pr == 0 then
-    error('fatal: could not detect pull request number.')
+    vim.notify('fatal: could not detect pull request number.', vim.log.levels.ERROR)
+    return
   end
 
   local target_remote = remote_base_url(args)
@@ -95,13 +79,11 @@ function gopr.open_git_pull_request(args)
   local target_url = 'https://github.com/' .. target_remote .. '/pull/' .. target_pr
 
   os.execute('open ' .. target_url)
-  print('opened: ' .. target_url .. ' on ' .. target_commit)
+  vim.notify('opened: ' .. target_url .. ' on ' .. target_commit)
 end
 
 -- open commit diff (and open pull request manually)
 function gopr.open_git_commit_diff(args)
-  check_git()
-
   local target_commit = current_commit_hash()
 
   local target_remote = remote_base_url(args)
@@ -109,7 +91,7 @@ function gopr.open_git_commit_diff(args)
   local target_url = 'https://github.com/' .. target_remote .. '/commit/' .. target_commit
 
   os.execute('open ' .. target_url)
-  print('opened: ' .. target_url)
+  vim.notify('opened: ' .. target_url)
 end
 
 return gopr
